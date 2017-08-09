48f6a3752472b1acfa4e94c78c2abddbb90e9417
Алена Шевчук <aliashevchuk95@gmail.com>
2017-08-10 00:41:01 +0300

    Initial commit.

---
+++ regedit/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace regedit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(283, 368);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(109, 48);
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registryToolStripMenuItem});
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.createToolStripMenuItem.Text = "Create";
+            // 
+            // registryToolStripMenuItem
+            // 
+            this.registryToolStripMenuItem.Name = "registryToolStripMenuItem";
+            this.registryToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.registryToolStripMenuItem.Text = "Registry";
+            this.registryToolStripMenuItem.Click += new System.EventHandler(this.registryToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(289, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(531, 368);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 369);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ regedit/Form1.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace regedit
+{
+    public partial class Form1 : Form
+    {
+        private TreeNode root;
+        private TreeNode KeyToNode(RegistryKey key)
+        {
+            return KeyToNode(key, key.Name);
+        }
+        private TreeNode KeyToNode(RegistryKey key, string shortName)
+        {
+            var node = new TreeNode(shortName, 0, 1);
+            node.Tag = key;
+            if (key.SubKeyCount > 0)
+            {
+                foreach (var subkey in key.GetSubKeyNames())
+                {
+                    try
+                    {
+                        node.Nodes.Add(GetNode(key.OpenSubKey(subkey), subkey));
+                    }
+                    catch (System.Security.SecurityException exc)
+                    {
+                        var restrictedNode = new TreeNode(subkey, 3, 3);
+                        restrictedNode.ToolTipText = exc.Message;
+                        node.Nodes.Add(restrictedNode);
+                    }
+                }
+            }
+            return node;
+        }
+        private TreeNode GetNode(RegistryKey key, string shortname)
+        {
+            var node = new TreeNode(shortname);
+            node.Tag = key;
+            foreach (var subkey in key.GetSubKeyNames())
+            {
+                try
+                {
+                    node.Nodes.Add(GetNode(key.OpenSubKey(subkey), subkey));
+                }
+                catch (System.Security.SecurityException exc)
+                {
+                    var restrictedNode = new TreeNode(subkey, 3, 3);
+                    restrictedNode.ToolTipText = exc.Message;
+                    node.Nodes.Add(restrictedNode);
+                }
+            }
+            return node;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            root = new TreeNode("Computer", 2, 2);
+            //root.Nodes.Add(KeyToNode(Registry.ClassesRoot));
+            root.Nodes.Add(KeyToNode(Registry.CurrentUser));
+            //root.Nodes.Add(KeyToNode(Registry.LocalMachine));
+            root.Nodes.Add(KeyToNode(Registry.Users));
+            root.Nodes.Add(KeyToNode(Registry.CurrentConfig));
+            treeView1.Nodes.Add(root);
+            root.Expand();
+            dataGridView1.Columns.Add("columnName", "Name");
+            dataGridView1.Columns.Add("columnType", "Type");
+            dataGridView1.Columns.Add("columnData", "Data");
+        }
+        private void registryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            RegistryKey key = null;
+            if (string.IsNullOrEmpty(node.FullPath))
+                return;
+            string[] pathElements = node.FullPath.Split(new[] { '\\' });
+            string subPath = node.FullPath.IndexOf('\\') == -1 ? node.FullPath : node.FullPath.Substring(node.FullPath.IndexOf('\\'));
+            if (pathElements[1] == Registry.ClassesRoot.Name)
+                key = Registry.ClassesRoot;
+            else if (pathElements[1] == Registry.CurrentUser.Name)
+                key = Registry.CurrentUser;
+            else if (pathElements[1] == Registry.LocalMachine.Name)
+                key = Registry.LocalMachine;
+            else if (pathElements[1] == Registry.Users.Name)
+                key = Registry.Users;
+            subPath = subPath.Replace("\\" + key.Name + "\\", "");
+            using (RegistryKey rk = key.OpenSubKey(subPath, true))
+            {
+                int k = rk.SubKeyCount;
+                rk.CreateSubKey("New registry #" + (k + 1).ToString());
+                treeView1.SelectedNode.Nodes.Add("New registry #" + (k + 1).ToString());
+            }
+        }
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                treeView1.SelectedNode = e.Node;
+                this.contextMenuStrip1.Show(treeView1, e.Location);
+            }
+        } 
+        private string chooseType(RegistryValueKind value)
+        {
+            switch (value.ToString())
+            {
+                case "String":
+                    return "REG_SZ";
+                case "ExpandString":
+                    return "REG_EXPAND_SZ";
+                case "Binary":
+                    return "REG_BINARY";
+                case "DWord":
+                    return "REG_DWORD";
+                case "MultiString":
+                    return "REG_MULTI_SZ";
+                case "QWord":
+                    return "REG_QWORD";
+                case "Unknown":
+                    return "0";
+                default:
+                    return "unchecked((int)0xFFFFFFFF)";
+            }
+        }
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            RegistryKey key = null;
+            if (string.IsNullOrEmpty(node.FullPath))
+                return;
+            string[] pathElements = node.FullPath.Split(new[] { '\\' });
+            string name = pathElements[pathElements.Length - 1];
+            string subPath = node.FullPath.IndexOf('\\') == -1 ? node.FullPath : node.FullPath.Substring(node.FullPath.IndexOf('\\'));
+            if (pathElements[1] == Registry.ClassesRoot.Name)
+                key = Registry.ClassesRoot;
+            else if (pathElements[1] == Registry.CurrentUser.Name)
+                key = Registry.CurrentUser;
+            else if (pathElements[1] == Registry.LocalMachine.Name)
+                key = Registry.LocalMachine;
+            else if (pathElements[1] == Registry.Users.Name)
+                key = Registry.Users;
+            subPath = subPath.Replace("\\" + key.Name + "\\", "");
+            using (RegistryKey rk = key.OpenSubKey(subPath, true))
+            {
+                if (rk.SubKeyCount == 0)
+                {
+                    key.DeleteSubKeyTree(name, false);
+                    node.Remove();
+                }
+                else
+                {
+                    var keys = rk.GetSubKeyNames();
+                    foreach (var n in keys)
+                        rk.DeleteSubKeyTree(n, false);
+                    key.DeleteSubKeyTree(name, false);
+                    node.Remove();
+                }
+            }
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            this.dataGridView1.Rows.Clear();
+            RegistryKey key = null;
+            if (string.IsNullOrEmpty(e.Node.FullPath))
+                return;
+            string[] pathElements = e.Node.FullPath.Split(new[] { '\\' });
+            string rootKeyName = pathElements[0];
+            string subPath = e.Node.FullPath.IndexOf('\\') == -1 ? e.Node.FullPath : e.Node.FullPath.Substring(e.Node.FullPath.IndexOf('\\'));
+            if (pathElements.Length >= 2)
+            {
+                if (pathElements[1] == Registry.ClassesRoot.Name)
+                    key = Registry.ClassesRoot;
+                else if (pathElements[1] == Registry.CurrentUser.Name)
+                    key = Registry.CurrentUser;
+                else if (pathElements[1] == Registry.LocalMachine.Name)
+                    key = Registry.LocalMachine;
+                else if (pathElements[1] == Registry.Users.Name)
+                    key = Registry.Users;
+                if (pathElements.Length > 2)
+                {
+                    try
+                    {
+                        subPath = subPath.Replace("\\" + key.Name + "\\", "");
+                        key = key.OpenSubKey(subPath);
+                        foreach (string valueName in key.GetValueNames())
+                            this.dataGridView1.Rows.Add(new object[] { valueName, chooseType(key.GetValueKind(valueName)), key.GetValue(valueName).ToString() });
+                        key.Close();
+                    }
+                    catch (System.Security.SecurityException exc)
+                    {
+                        MessageBox.Show(exc.Message);
+                    }
+                }
+            }
+        }
+    }
+}
